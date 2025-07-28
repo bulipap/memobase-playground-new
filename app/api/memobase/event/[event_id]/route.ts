38b7fed8b563103f11dbbd6ec32b799ff62677d5
bulipap <emanuel.jhutti@gmail.com>
2025-07-28 13:47:07 +0200

    Update route.ts

--- app/api/memobase/event/[event_id]/route.ts
+++ app/api/memobase/event/[event_id]/route.ts
@@ -1,27 +1,34 @@
 import { createApiResponse, createApiError } from "@/lib/api-response";
 import { createClient } from "@/utils/supabase/server";
 import { memoBaseClient } from "@/utils/memobase/client";
-import type { NextRequest } from "next/server";
-import type { NextApiRequest } from "next"; // optional
 
+/**
+ * 删除 event
+ * @param event_id event ID
+ */
 export async function DELETE(
-  req: NextRequest,
+  req: Request,
   context: { params: { event_id: string } }
 ) {
-  const { event_id } = context.params;
-
+  // get user from supabase
   const supabase = await createClient();
   const { data, error } = await supabase.auth.getUser();
   if (error || !data?.user) {
     return createApiError("未授权", 401);
   }
 
+  const { event_id } = context.params;
+  if (!event_id) {
+    return createApiError("Bad Request", 400);
+  }
+
   try {
     const user = await memoBaseClient.getOrCreateUser(data.user.id);
     await user.deleteEvent(event_id);
-    return createApiResponse(null, "删除成功");
-  } catch (err) {
-    console.error(err);
+  } catch (error: unknown) {
+    console.error(error);
     return createApiError("删除失败", 500);
   }
+
+  return createApiResponse(null, "删除成功");
 }
